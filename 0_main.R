622e68766e4487370aecbc1059c97e50b3395420
dr-rosen-rosen <mrosen44@jhmi.edu>
2021-06-29 14:08:35 -0400

    More updates

--- 0_main.R
+++ 0_main.R
@@ -1,1 +1,16 @@
 ### Main project file
+
+library(config)
+library(tidyverse)
+library(reticulate)
+library(here)
+
+debuggingState(on=FALSE)
+# start ve with: source python3/bin/activate in project folder
+Sys.setenv(R_CONFIG_ACTIVE = "default") # 'default')#
+config <- config::get()
+Sys.setenv(RETICULATE_PYTHON = config$py_version)
+reticulate::source_python('1_funcs.py')
+source(here('1_funcs.R'), echo = TRUE)
+#source(here('2a_connect.R'), echo = TRUE)
+
---
+++ 1_funcs.py
@@ -0,0 +1,1 @@
+####### python scripts for PICU_CAL
